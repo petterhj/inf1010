f92b200afcba7a53e86a7ee14204c29e7723fc32
Petter H. Juliussen <post@petterhj.net>
2014-01-23 09:35:38 +0000

    cleanup

--- oblig1/Oblig1.java
+++ oblig1/Oblig1.java
@@ -1,3 +1,8 @@
+/*
+     INF1010 - Oblig 1
+    ---------------------------
+     petterju
+*/
 class Oblig1 {
     public static void main(String[] args) {
         Tester test = new Tester();
@@ -6,12 +11,13 @@ class Oblig1 {
 }
 
 
+// Class: Tester
 class Tester {
     Person ask = new Person("Ask", 3);
     Person dana = new Person("Dana", 3);
     Person tom = new Person("Tom", 3);
     Person brynjulf = new Person("Brynjulf", 3);
-	
+
     public void kjor() {
         // Student 1: Ask
         ask.blirKjentMed(dana);
@@ -59,6 +65,7 @@ class Tester {
 }
 
 
+// Class: Person
 class Person {
     // Variables
     private String navn;
@@ -66,26 +73,25 @@ class Person {
     private Person[] likerikke;
     private Person forelsketi;
     private Person sammenmed;
-    
-	
+
     // Constructor
-    Person(String n, int lengde) {
+    Person (String n, int lengde) {
         this.navn = n;
 
         this.kjenner = new Person[lengde];
         this.likerikke = new Person[lengde];
     }
-	
+
     // Return name
     public String hentNavn() {
         return this.navn;
     }
-    
+
     // Check if acquainted
     public boolean erKjentMed(Person p) {
         return inArray(p, kjenner);
     }
-    
+
     // Add acquaintance
     public void blirKjentMed(Person p) {
         if (!this.erSegSelv(p))
@@ -95,12 +101,12 @@ class Person {
                     break;
                 }
     }
-    
+
     // Set love interest
     public void blirForelsketI(Person p) {
         if (!this.erSegSelv(p)) this.forelsketi = p;
     }
-    
+
     // Set acquaintance as enemy
     public void blirUvennMed(Person p) {
         if (!this.erSegSelv(p))
@@ -110,13 +116,13 @@ class Person {
                     break;
                 }
     }
-    
+
     // Check if friend
     public boolean erVennMed(Person p) {
         if (inArray(p, kjenner) && !inArray(p, likerikke)) return true;
         return false;
     }
-    
+
     // Set as friend (remove from enemies)
     public void blirVennMed(Person p) {
         for (int i = 0; i < this.likerikke.length; i++)
@@ -126,80 +132,80 @@ class Person {
                 break;
             }
     }
-    
+
     // Print friend list
     public void skrivUtVenner() {
         System.out.println("Venner:");
-		
+
         for (Person p : this.hentVenner())
             System.out.println(" > " + p.hentNavn());
     }
-    
+
     // Return best friend
     public Person hentBestevenn() {
         Person[] venner = this.hentVenner();
         return venner[0];
     }
-    
+
     // Return friends
     public Person[] hentVenner() {
         Person[] venner = new Person[this.antVenner()];
         int vi = 0;
-		
+
         for (int i = 0; i < this.kjenner.length; i++)
             if (this.erVennMed(this.kjenner[i]))
                 venner[vi++] = this.kjenner[i];
-				
+
         return venner;
     }
-    
+
     // Return friend count
     public int antVenner() {
         int ant = 0;
-		
+
         for (Person p : this.kjenner)
             if (this.erVennMed(p)) ant++;
-			
+
         return ant;
     }
-    
+
     // Print acquaintances
     public void skrivUtKjenninger() {
         for (Person p : this.kjenner)
             if (p != null) System.out.print(p.hentNavn( ) + " ");
         System.out.println("");
     }
-    
+
     // Print enemies
     public void skrivUtLikerIkke( ) {
         for (Person p : this.likerikke)
             if (p != null) System.out.print(p.hentNavn( ) + " ");
         System.out.println("");
     }
-    
+
     // Print profile
     public void skrivUtAltOmMeg ( ) {
         System.out.print("--- " + this.navn + " ");
         for (int i = 0; i < (50 - this.navn.length()); i++) System.out.print("-");
         System.out.print("\n > Kjenner:\t\t");
         skrivUtKjenninger();
-        
+
         if(forelsketi != null)
             System.out.println(" > Er forelsket i\t" + this.forelsketi.hentNavn());
-        
+
         System.out.print(" > Liker ikke:\t\t");
         skrivUtLikerIkke();
-		
+
         System.out.println();
     }
-    
+
     // Check if person in array
     private boolean inArray(Person n, Person[] h) {
         for (Person p : h)
             if (p == n) return true;
         return false;
     }
-	
+
     // Check if self
     private boolean erSegSelv(Person p) {
         if (this == p) return true;
