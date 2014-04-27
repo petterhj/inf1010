66ab301f735da3ef2a64f5d0a085fe71c4c68dcd
Petter H. Juliussen <post@petterhj.net>
2014-04-27 20:45:21 +0000

    update

--- oblig5/Felt.java
+++ oblig5/Felt.java
@@ -66,7 +66,7 @@ class Felt {
 			if (r instanceof StatiskRute)
 				rad += cR + v + cW;
 			if (r instanceof VariabelRute) {
-				rad += v;
+				rad += cR + v + cW;
 			}
 			if ((i+1)%r.hentBoks().hentAntallKolonner()==0)
 				rad += cB + " |" + cW;
