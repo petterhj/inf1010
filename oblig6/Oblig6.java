b1177691810a38e4353926b5a1ade29b20c403d8
Petter H. Juliussen <post@petterhj.net>
2014-05-08 08:12:46 +0000

    files

---
+++ oblig6/Oblig6.java
@@ -0,0 +1,14 @@
+/*
+	INF1010 - v14 - Oblig6
+	petterhj
+*/
+
+
+//	Class: Oblig6
+// =================================================================================
+class Oblig6 {
+	// Main
+	public static void main(String[] args) {
+
+	}
+}
