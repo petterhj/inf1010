28a9e6432db2312d613efd51e0e1dd5414b7e235
Petter H. Juliussen <post@petterhj.net>
2014-05-09 15:43:02 +0000

    removed some old files

--- oblig6/Oblig6.java
+++ oblig6/Oblig6.java
@@ -4,11 +4,65 @@
 */
 
 
+// Imports
+import java.io.File;
+
+
+
 //	Class: Oblig6
 // =================================================================================
 class Oblig6 {
 	// Main
 	public static void main(String[] args) {
+		// Header
+		System.out.println("\n========================================================");
+		System.out.println(" Oblig 6 - WordSorter");
+		System.out.println("========================================================\n");
 
+		// Arguments
+		if (args.length < 3) {
+			// Usage
+			System.out.println(" - Usage: Oblig6 <thread count> <input file> <output file>");
+		}
+		else {
+			// Validate arguments
+			System.out.println(" - Checking arguments...");
+
+			boolean validThreadCnt = false;
+			int threadCnt = 0;
+			File input = new File(args[1]);
+			File output = new File(args[2]);
+
+			try {
+				// Thread count
+        		threadCnt = Integer.parseInt(args[0]);
+
+        		if (threadCnt > 0)
+        			validThreadCnt = true;
+    		}
+    		catch (NumberFormatException e) {
+    			validThreadCnt = false;
+			}
+
+			if (validThreadCnt) {
+	    		// Input file
+				if (input.isFile()) {
+					System.out.println("  > Threads:\t" + args[0]);
+					System.out.println("  > Input:\t" + args[1]);
+					System.out.println("  > Output:\t" + args[2] + "\n");
+
+					// Sort words
+					new WordSorter(threadCnt, input, output);
+				}
+				else
+					System.out.println(" - Input file not found!");
+			}
+			else {
+				System.out.println(" - Please provide a valid positive integer for argument \"thread count\"!");
+			}
+		}
+
+		// Footer
+		System.out.println("\n========================================================\n");
 	}
 }
